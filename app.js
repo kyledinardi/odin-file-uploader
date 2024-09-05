62dba316a5ff4d5a0115d41c2a05e32001a66757
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-09-05 14:03:30 -0400

    Set up express

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const passport = require('passport');
 const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.session());
+require('./passport');
+
 app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Page not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(PORT, () =>
   console.log(`My first Express app - listening on port ${PORT}!`),
